897b0ee702797f7de54e49ebb70bca83db487b36
Szymon D <szymon.dabrowski@cyfroweszkoly.pl>
2026-03-04 18:46:36 +0100

    analisys czesc2

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -5,3 +5,12 @@ const gatunek = db.movies.find({
     genres: "Action"
 })
 printjson(gatunek);
+
+print("$gt i $and");
+const gtand = db.movies.find({
+    $and: [
+        { releaseDate: { $gt: new Date("2022-01-01") } },
+        { viewsCount: { $gt: 2000 } }
+    ]
+})
+printjson(gtand);
